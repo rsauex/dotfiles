ca02a48b6f754f32245d385ca1370877d718c50d
rsauex <yuragrig@ukr.net>
2020-06-10 20:49:09 +0300

    Base of direnv alternative for powershell

--- powershell/.config/powershell/profile.ps1
+++ powershell/.config/powershell/profile.ps1
@@ -56,7 +56,7 @@ function Add-PromptHook() {
 
 function Prompt {
     # Invoke hooks
-    $PWSH_PROMPT_HOOKS | ForEach { Invoke-Command -ScriptBlock $_ }
+    $PWSH_PROMPT_HOOKS | ForEach-Object { Invoke-Command -ScriptBlock $_ }
 
     ## Output actual prompt
     # First line
@@ -79,7 +79,7 @@ function Prompt {
 # ----- Tmux -----
 
 Add-PromptHook {
-    if (-Not (Test-Path env:TMUX)) {
+    if (-not (Test-Path env:TMUX)) {
         return
     }
     foreach ($line in (tmux show-environment)) {
@@ -93,3 +93,137 @@ Add-PromptHook {
         }
     }
 }
+
+# ----- Utils -----
+
+function Find-String() {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [String]$Pattern,
+
+        [Parameter(Mandatory=$false)]
+        [String[]]$Path,
+
+        [Parameter(Mandatory=$false)]
+        [Switch]$CaseSensitive
+    )
+
+    $arguments = @( )
+    if ($CaseSensitive) {
+        $arguments += '-s'
+    }
+    [string[]]$DefaultProperties = 'path', 'lines'
+    $ddps = New-Object System.Management.Automation.PSPropertySet DefaultDisplayPropertySet, $DefaultProperties
+    $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]$ddps
+
+    (&rg --json $arguments $pattern $path) | ConvertFrom-Json | Where-Object { $_.type -eq 'match' } `
+      | ForEach-Object { $_.data } | Add-Member -PassThru -MemberType MemberSet -Name PSStandardMembers -Value $PSStandardMembers
+}
+
+# ----- DirEnv -----
+
+class DirEnvAddition {
+    [string]$Path
+
+    DirEnvAddition($Path) {
+        $this.Path = $Path
+    }
+
+    [void] Revert() {
+        Remove-Item $this.Path
+    }
+}
+
+class DirEnvChange {
+    [string]$Path
+    $OldValue
+
+    DirEnvChange($Path, $OldValue) {
+        $this.Path = $Path
+        $this.OldValue = $OldValue
+    }
+
+    [void] Revert() {
+        Set-Item $this.Path $this.OldValue
+    }
+}
+
+class DirEnvDiff {
+    $Changes
+
+    DirEnvDiff([hashtable]$old, [hashtable]$new) {
+        $this.Changes = @( )
+        $old.GetEnumerator() | ForEach-Object {
+            if (-not $new.ContainsKey($_.Key)) {
+                $this.Changes += [DirEnvChange]::new($_.Key, $_.Value)
+            } elseif (-not $_.Value.Equals($new[$_.Key])) {
+                $this.Changes += [DirEnvChange]::new($_.Key, $_.Value)
+            }
+        }
+        $new.GetEnumerator() | ForEach-Object {
+            if (-not $old.ContainsKey($_.Key)) {
+                $this.Changes += [DirEnvAddition]::new($_.Key)
+            }
+        }
+    }
+}
+
+function Get-EnvironmentDump() {
+    [hashtable]$Dump = @{ }
+    Get-Variable -Scope 'global' | ForEach-Object {
+        if ($null -ne $_.PSPath) {
+            $Dump[$_.PSPath] = (Get-Content $_.PSPath)
+        }
+    }
+    Get-ChildItem -Path Env: | ForEach-Object {
+        $Dump[$_.PSPath] = (Get-Content $_.PSPath)
+    }
+    Get-ChildItem -Path Function: | ForEach-Object {
+        $Dump[$_.PSPath] = (Get-Content $_.PSPath)
+    }
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::$")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::?")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::^")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::_")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::args")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::CurrentlyExecutingCommand")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::Error")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::Event")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::EventArgs")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::EventSubscriber")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::ExecutionContext")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::false")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::foreach")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::input")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::LastExitCode")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::Matches")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::MyInvocation")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::NestedPromptLevel")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::null")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSBoundParameters")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSCmdlet")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSCommandPath")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSCulture")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSDebugContext")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSItem")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSScriptRoot")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSSenderInfo")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSUICulture")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PSVersionTable")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::PWD")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::Sender")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::ShellId")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::StackTrace")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::switch")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::this")
+    $Dump.Remove("Microsoft.PowerShell.Core\Variable::true")
+    return $Dump
+}
+
+function Invoke-WithDiff([ScriptBlock]$Block) {
+    $Old = (Get-EnvironmentDump)
+    Invoke-Command -ScriptBlock $Block
+    return [DirEnvDiff]::new($Old, (Get-EnvironmentDump))
+}
+
