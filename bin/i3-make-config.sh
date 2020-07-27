28301b803e3a0374a846aee9b306fe82c1a0b7f1
rsauex <yuragrig@ukr.net>
2020-07-27 14:17:09 +0300

    Make i3 config more machine-independent using M4 macro preprocessor.

--- bin/i3-make-config.sh
+++ bin/i3-make-config.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Remove old version
-rm -f $XDG_CONFIG_HOME/i3/config
-# Create an empty file
-touch $XDG_CONFIG_HOME/i3/config
+CONFIG_FILE="$XDG_CONFIG_HOME/i3/config"
+
+trap 'rm -f "$TMP_CONFIG_FILE"' EXIT
+
+TEMP_CONFIG_FILE=$(mktemp) || exit 1
 
 for f in $XDG_CONFIG_HOME/i3/[0-9][0-9]*.conf; do
-    cat $f >> $XDG_CONFIG_HOME/i3/config
+    cat $f >> $TEMP_CONFIG_FILE
 done
+
+if ! command -v m4 &> /dev/null; then
+    echo 'M4 macro preprocessor is missing!' >&2
+    exit 1
+fi
+
+M4_VARIABLES="-DPC_TYPE=$(cat /sys/class/dmi/id/chassis_type)"
+
+m4 $M4_VARIABLES $TEMP_CONFIG_FILE > $CONFIG_FILE
