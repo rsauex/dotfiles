952384e42dd7bc3c721bc3ea559b67aa25ecdde5
rsauex <yuragrig@ukr.net>
2021-12-29 22:08:17 +0200

    Use exec.

--- bin/i3-launch-or-select.sh
+++ bin/i3-launch-or-select.sh
@@ -12,7 +12,7 @@ escaped_window_name=$(echo "$window_name" | sed 's/\\/\\\\/g' | sed 's/\"/\\\"/g
 if [[ ! -z "$window_name" ]]; then
     window_id=`i3-msg -t get_tree | jq -r "[recurse(.nodes[], .floating_nodes[]) | select(.name == \"$escaped_window_name\") | .id] | first"`
     if [[ "$window_id" == "null" ]]; then
-        $PROGRAM "${ARGS[@]}" "$window_name"
+        exec $PROGRAM "${ARGS[@]}" "$window_name"
     else
         i3-msg "[con_id=${window_id}] focus" > /dev/null
     fi
