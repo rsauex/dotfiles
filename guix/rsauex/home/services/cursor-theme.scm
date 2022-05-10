dddf343da4a219b5b422fcc7f899f71231a8b60d
rsauex <yuragrig@ukr.net>
2022-05-10 15:27:01 +0300

    Add cursor theme guix home service.

---
+++ guix/rsauex/home/services/cursor-theme.scm
@@ -0,0 +1,44 @@
+(define-module (rsauex home services cursor-theme)
+  #:use-module (gnu home)
+  #:use-module (gnu packages)
+  #:use-module (gnu services)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (gnu home services)
+  #:use-module (gnu services configuration)
+
+  #:export (cursor-theme-configuration
+
+            cursor-theme-configuration-theme-name
+            cursor-theme-configuration-theme-package
+
+            cursor-theme-service-type))
+
+(define-maybe package)
+(define-maybe string)
+
+(define-configuration cursor-theme-configuration
+  (theme-package
+   (maybe-package 'disabled)
+   "The cursor theme package.")
+  (theme-name
+   (maybe-string 'disabled)
+   "The cursor theme name."))
+
+(define (add-cursor-theme-configuration config)
+  (if (or (eq? 'disabled (cursor-theme-configuration-theme-package config))
+          (eq? 'disabled (cursor-theme-configuration-theme-name config)))
+      `()
+      `((".icons/default"
+         ,(file-append (cursor-theme-configuration-theme-package config)
+                       "/share/icons/"
+                       (cursor-theme-configuration-theme-name config))))))
+
+(define cursor-theme-service-type
+  (service-type (name 'cursor-theme)
+                (extensions
+                 (list (service-extension
+                        home-files-service-type
+                        add-cursor-theme-configuration)))
+                (default-value (cursor-theme-configuration))
+                (description "Install and set default cursor theme.")))
