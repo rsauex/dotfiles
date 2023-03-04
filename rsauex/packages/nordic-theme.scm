b6fdc7f51867633803189d6191abe9b6ee05a5de
rsauex <rsauex@mailbox.org>
2023-03-04 13:33:26 +0200

    Bum nordic kvantum theme version and disable transparency.

--- rsauex/packages/nordic-theme.scm
+++ rsauex/packages/nordic-theme.scm
@@ -35,30 +35,38 @@ palette.")
    (license license:gpl3))))
 
 (define-public nordic-darker-kvantum-theme
-  (let ((commit "07d764c5ebd5706e73d2e573f1a983e37b318915")
-	(revision "0"))
-    (package
-      (name "nordic-darker-kvantum-theme")
-      (version (git-version "1.9.0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/EliverLara/Nordic")
-               (commit commit)))
-         (sha256
-          (base32
-           "0y2s9d6h1b195s6afp1gb5rb1plfslkpbw2brd30a9d66wfvsqk0"))
-         (file-name (git-file-name name version))))
-      (build-system copy-build-system)
-      (arguments
-       `(#:install-plan
-         `(("./kde/kvantum/Nordic-Darker" "share/Kvantum/Nordic-Darker"))))
-      (home-page "https://github.com/EliverLara/Nordic")
-      (synopsis "Dark Gtk3.20+ theme using the Nord color palette")
-      (description "Nordic is a Gtk3.20+ theme created using the Nord color
+  (package
+    (name "nordic-darker-kvantum-theme")
+    (version "2.2.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/EliverLara/Nordic")
+             (commit "v2.2.0")))
+       (sha256
+        (base32
+         "02aa1dphzjfy1fi7ln4554di24fa0kcgz8baabm15m9zm9sqfdf1"))
+       (file-name (git-file-name name version))))
+    (build-system copy-build-system)
+    (arguments
+     `(#:install-plan
+       `(("./kde/kvantum/Nordic-Darker" "share/Kvantum/Nordic-Darker"))
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'disable-transparency
+           (lambda _
+             (substitute* `("./kde/kvantum/Nordic-Darker/Nordic-Darker.kvconfig")
+               (("translucent_windows=true")
+                "translucent_windows=false")
+               (("composite=true")
+                "composite=false"))
+             #t)))))
+    (home-page "https://github.com/EliverLara/Nordic")
+    (synopsis "Dark Gtk3.20+ theme using the Nord color palette")
+    (description "Nordic is a Gtk3.20+ theme created using the Nord color
 palette.")
-      (license license:gpl3))))
+    (license license:gpl3)))
 
 (define-public rofi-nord-theme
   (let ((commit "71c76dcf31e38b426519198276cf6b5ba626e61c"))
