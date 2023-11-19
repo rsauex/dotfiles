970b7747d38f1a7c11d2e7412553878a227cb59d
rsauex <rsauex@mailbox.org>
2023-11-19 17:15:10 +0200

    Improve template engine: allow passing arguments

--- rsauex/script/template.scm
+++ rsauex/script/template.scm
@@ -44,12 +44,9 @@
   (lexeme! #:char char))
 
 (define (on-rbrace)
-  (let ((next-char (read-char)))
-    (cond
-     ((char=? next-char #\#)
-      #t)
-     (#t
-      (error "Expected #")))))
+  (case (read-char)
+    ((#\#) #t)
+    (else (error "Expected #"))))
 
 (define (read-expr-type)
   (case (read-char)
@@ -57,40 +54,37 @@
     ((#\!) #:expr-sans-output)
     (else (error "Expected #, !, or $"))))
 
-(define (read-scheme)
-  (let ((expr-type (read-expr-type)))
-    (lexeme! expr-type (read)))
+(define (read-scheme-rest expr-type)
+  (lexeme! expr-type (read))
   (while (char-set-contains? char-set:whitespace (peek-char))
     (read-char))
-  (let ((next-char (read-char)))
-    (cond
-     ((char=? next-char #\})
-      (on-rbrace))
-     (#t
-      (error "Expected }")))))
+  (case (peek-char)
+    ((#\})
+     (read-char)
+     (on-rbrace))
+    (else
+     (if (eq? #:expr-with-output expr-type)
+         (error "Expected }")
+         (read-scheme-rest expr-type)))))
 
 (define (on-sharp)
-  (let ((next-char (read-char)))
-    (cond
-     ((char=? next-char #\{)
-      (read-scheme))
-     (#t
-      (char! #\#)
-      (char! next-char)))))
+  (case (peek-char)
+    ((#\{)
+     (read-char)
+     (read-scheme-rest (read-expr-type)))
+    (else
+     (char! #\#)
+     (char! (read-char)))))
 
 (define (port->lexemes port)
   (call-in-lexer
    (lambda _
      (with-input-from-port port
        (lambda _
-         (let ((next-char (read-char)))
-           (while (not (eof-object? next-char))
-             (cond
-              ((char=? next-char #\#)
-               (on-sharp))
-              (#t
-               (char! next-char)))
-             (set! next-char (read-char)))))))))
+         (while (not (eof-object? (peek-char)))
+           (case (read-char)
+             ((#\#) (on-sharp))
+             (else => char!))))))))
 
 (define (string->lexemes str)
   (call-with-input-string str
@@ -124,13 +118,14 @@
          (code (lexemes->code lexemes)))
     (eval code (resolve-module '(rsauex script template eval)))))
 
-(define (rsauex-home-template-file file name)
-  (define template-gexp
-    #~(begin
-        (use-modules (ice-9 popen)
-                     (ice-9 textual-ports))
-        (with-fluids ((%default-port-encoding "UTF-8"))
-          (with-output-to-file #$output
-            (lambda _
-              #$(template-file->gexp file))))))
-  (computed-file name template-gexp))
+(define* (rsauex-home-template-file file name #:optional (args '()))
+  (parameterize ((template-eval-module:args args))
+    (define template-gexp
+      #~(begin
+          (use-modules (ice-9 popen)
+                       (ice-9 textual-ports))
+          (with-fluids ((%default-port-encoding "UTF-8"))
+            (with-output-to-file #$output
+              (lambda _
+                #$(template-file->gexp file))))))
+    (computed-file name template-gexp)))
--- rsauex/script/template/eval.scm
+++ rsauex/script/template/eval.scm
@@ -1,2 +1,9 @@
 (define-module (rsauex script template eval)
-  #:use-module ((guix gexp)))
+  #:use-module ((guix gexp))
+  #:use-module ((rsauex home config)))
+
+(define-public args
+  (make-parameter '()))
+
+(define (get-arg key)
+  (assq-ref (args) key))
