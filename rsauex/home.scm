7721032b22e048bd5f8062a906d0eea56fde860f
rsauex <rsauex@mailbox.org>
2024-06-23 10:27:25 +0300

    MS-like fonts.

--- rsauex/home.scm
+++ rsauex/home.scm
@@ -202,6 +202,7 @@
                    fonts:font-awesome
                    fonts:font-google-material-design-icons
                    fonts:font-terminus
+                   fonts:font-liberation
 
                    gnome:gnome-themes-standard
                    gnome:gnome-themes-extra
