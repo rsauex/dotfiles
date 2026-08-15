ebe9c845f077a8e35970cb04ef00a8fe8362f4a5
rsauex <rsauex@mailbox.org>
2026-08-15 19:02:16 +0300

    Make it possible to rewrite target/value-alist in make-anon-service

--- rsauex/services.scm
+++ rsauex/services.scm
@@ -1,21 +1,23 @@
 (define-module (rsauex services)
   #:use-module ((gnu services))
   #:use-module ((ice-9 match))
+  #:use-module ((srfi srfi-26))
 
   #:export (anon-service))
 
-(define (make-anon-service name target/value-list)
+(define (make-anon-service name target/value-alist)
+  ;; Store target/value-alist in the value for (@ (nonguix utils)
+  ;; with-transformation) to be able to rewrite it.
   (let* ((extensions (map (match-lambda
-                            ((target . value)
-                             (service-extension target (const value))))
-                          target/value-list))
-         (type (service-type (name name)
-                             (extensions extensions)
-                             (default-value #f)
-                             (description "This is a simple service."))))
-    (service type)))
+                            ((target . _value)
+                             (service-extension target (cut assq-ref <> target))))
+                          target/value-alist)))
+    (service (service-type (name name)
+                           (extensions extensions)
+                           (description (symbol->string name)))
+             target/value-alist)))
 
 (define-syntax anon-service
   (syntax-rules ()
     ((_ name (target value) ...)
-     (make-anon-service (quote name) (list  (cons target value) ...)))))
+     (make-anon-service (quote name) (list (cons target value) ...)))))
